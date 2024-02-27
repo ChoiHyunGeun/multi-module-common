1051b9dc08839ed7e1449d85360c146ac917967e
hgChoi <chlgusrms96@naver.com>
2024-02-27 15:28:33 +0900

    module-api에 의존성이 주입 되는지 확인하기 위해 적당한 enum 생성

---
+++ src/main/java/dev/be/modulecommon/enums/CodeEnum.java
@@ -0,0 +1,19 @@
+package dev.be.modulecommon.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum CodeEnum {
+    SUCCESS("0000", "SUCCESS"),
+    UNKNOWN_ERROR("9999", "UNKNOWN_ERROR");
+
+    private String code;
+    private String message;
+
+//    CodeEnum(String code, String message) {
+//        this.code = code;
+//        this.message = message;
+//    }
+}
