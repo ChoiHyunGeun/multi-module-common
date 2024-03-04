c3d32de956f2ae658523558444a3c7f0b8e97d67
hgChoi <chlgusrms96@naver.com>
2024-03-04 17:01:06 +0900

    빌드, 배포를 위한 설정 추가 / 멤버 서비스 로직 추가

---
+++ src/main/java/dev/be/modulecommon/domain/Member.java
@@ -0,0 +1,22 @@
+package dev.be.modulecommon.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+}
